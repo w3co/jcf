4b05f94ba61182111e9c347bad64428f5daf55a6
Sergey <inferpse@users.noreply.github.com>
2016-02-18 18:03:26 +0200

    bump new version

--- js/jcf.js
+++ js/jcf.js
@@ -4,7 +4,7 @@
  * Copyright 2014-2015 PSD2HTML - http://psd2html.com/jcf
  * Released under the MIT license (LICENSE.txt)
  *
- * Version: 1.2.0
+ * Version: 1.2.1
  */
 ;(function(root, factory) {
 	'use strict';
@@ -19,7 +19,7 @@
 	'use strict';
 
 	// define version
-	var version = '1.2.0';
+	var version = '1.2.1';
 
 	// private variables
 	var customInstances = [];
